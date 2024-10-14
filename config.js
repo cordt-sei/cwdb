0e8d3ff0ed9444e7a271f426e7276d34ac824a96
cordt-sei <165932662+cordt-sei@users.noreply.github.com>
2024-10-14 08:15:37 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ export const config = {
   blockHeight: 94496767,
   paginationLimit: 100,
   numWorkers: 4,
-  restAddress: "hlocalhost:1317",
-  wsAddress: "ws://tasty.seipex.fi/websocket",
-  evmRpcAddress: "http://tasty.seipex.fi:8545",
+  restAddress: "http://localhost:1317",
+  wsAddress: "ws://localhost:26657/websocket",
+  evmRpcAddress: "http://localhost:8545",
   pointerApi: "https://pointer.basementnodes.ca",
   timeout: 5000 // Timeout in milliseconds
 };
